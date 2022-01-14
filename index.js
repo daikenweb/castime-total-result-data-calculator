882f2edaeda4ce98efd22f026189152461d98ffa
FORNO <forno@xmaho.link>
2022-01-14 14:34:36 +0900

    Remove body_start
    Signed-off-by: FORNO <forno@xmaho.link>

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ import moment from "moment"
  * @param {*} parameters
  * @returns function for CSV_Alone.Output on ajax
  */
-export const calculateTotalResultData = ({output_type, start_date, end_date, body_start}) =>
+export const calculateTotalResultData = ({output_type, start_date, end_date}) =>
   (res) => {
     console.log("取得確認",res);
+    const body_start = moment(start_date).format("YYYY-MM-DD");
 
     //console.log("集計開始月",start_date);
     //console.log("開始設定",res["user_data"]["work_begin"]["month_decision"]);
