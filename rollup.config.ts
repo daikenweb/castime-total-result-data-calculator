8d25b2e2df96b16e524b275e2490a8290c70a7c4
FORNO <forno@xmaho.link>
2022-01-27 13:26:34 +0900

    Add rollup build
    Signed-off-by: FORNO <forno@xmaho.link>

---
+++ rollup.config.ts
@@ -0,0 +1,39 @@
+import pluginTypescript from "@rollup/plugin-typescript";
+import pluginNodePolyfills from "@rollup/plugin-node-resolve"
+import * as pkg from "./package.json";
+
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.homepage}
+ * Copyright (c) ${new Date().getFullYear()} ${pkg.author.name}
+ * Licensed under the ${pkg.license} license.
+ */`;
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      name: "index.js",
+      file: pkg.module,
+      format: "es",
+      sourcemap: "inline",
+      banner,
+    },
+    plugins: [
+      pluginTypescript({
+        tsconfig: "./tsconfig.json",
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      name: "castime-total-result-data-calculator",
+      file: pkg.browser,
+      format: "es",
+      sourcemap: "inline",
+      banner,
+    },
+    plugins: [pluginTypescript(), pluginNodePolyfills()],
+  },
+];
